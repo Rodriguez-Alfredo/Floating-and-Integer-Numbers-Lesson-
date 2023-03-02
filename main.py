b5e1c4c7bf149c26f5ed0cc22cbfcf018714ce0e
AlfredoRodriguez <126662592+Rodriguez-Alfredo@users.noreply.github.com>
2023-03-02 06:01:41 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+#Integers & Floating Points
+
+#flotaing point is a decimal number
+print(4.0)
+
+#integer has NO decimal point
+print(4)
+
+#numbers in variable
+x = 4
+y = 4.0
+
+#adding '+' will add numbers
+print(x + y)
+
+#converting floating numbers to integers with function 'int'
+print(int(4.0))
+
+#convert integer to a floating number with function 'float'
+print(float(4))
